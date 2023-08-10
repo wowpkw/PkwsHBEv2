593ce1751e9f1e0691f24b34f1462355f50e0e1e
Pkw <104768274+wowpkw@users.noreply.github.com>
2023-08-11 01:30:36 +0200

    Create SupportedGames.lua

---
+++ SupportedGames.lua
@@ -0,0 +1,8 @@
+Items = {
+		"Dynasty Battlegrounds",
+		"Alternate Battlegrounds",
+		"Black Magic II",
+		"Be A Parkour Ninja",
+		"Da Hood",
+		"Pretty much any other game since it's universal."
+	},
