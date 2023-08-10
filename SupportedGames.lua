28d3ec3bd3a75feae8e27c1f078a1f6ca5c51f44
Pkw <104768274+wowpkw@users.noreply.github.com>
2023-08-11 01:31:59 +0200

    Update SupportedGames.lua

--- SupportedGames.lua
+++ SupportedGames.lua
@@ -1,8 +1,9 @@
-Items = {
+local games = {
 		"Dynasty Battlegrounds",
 		"Alternate Battlegrounds",
 		"Black Magic II",
 		"Be A Parkour Ninja",
 		"Da Hood",
 		"Pretty much any other game since it's universal."
-	},
+}
+return games
