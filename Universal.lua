43cf706486178e343b80bb3677febc1af37ba91c
Pkw <104768274+wowpkw@users.noreply.github.com>
2023-08-11 01:04:10 +0200

    Create Universal.lua

---
+++ Universal.lua
@@ -0,0 +1,156 @@
+-- other
+_G.Version = "1.0.3" -- VERSION (if you plan to update)
+local httpService = game:GetService("HttpService")
+local discord = "https://discord.gg/UTWMshSKzy"
+-- gui creation
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+
+local GUI = Mercury:Create{
+    Name = "Pkw's U.HBE",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Legacy,
+    Link = "https://github.com/wowpkw/PkwsHBE/tree"
+}
+
+local Tab = GUI:Tab{
+	Name = "Universal-HBE-V".._G.Version,
+	Icon = "rbxassetid://3173271667"
+}
+local Tab2 = GUI:Tab{
+	Name = "Settings",
+	Icon = "rbxassetid://8382597378"
+}
+local Tab3 = GUI:Tab{
+	Name = "Supported-Games",
+	Icon = "rbxassetid://7461510428"
+}
+local Tab4 = GUI:Tab{
+	Name = "Report-Bugs",
+	Icon = "rbxassetid://7562374548"
+}
+
+GUI:Credit{
+	Name = "pkw",
+	Description = "Made the script",
+	Discord = "pkw.",
+}
+
+Tab:prompt{
+            Title = "Join discord?",
+            Text = "Acess to update logs, script announcements, community-shared scripts, etc.",
+            Buttons = {
+            Yes = function()
+            GUI:Notification{
+	Title = "Discord information",
+	Text = "discord.gg/UTWMshSKzy",
+	Duration = 25,
+	Callback = function() end
+}
+            end,
+            No = function()
+            end
+            }}
+
+-- buttons, functions, etc.
+Tab:Slider{
+	Name = "Hitbox Size",
+	Default = 1.1,
+	Min = 1.1,
+	Max = 20,
+	Callback = function(value) 
+	_G.HeadSize = value
+	end
+}
+
+Tab2:ColorPicker{
+	Style = Mercury.ColorPickerStyles.Legacy,
+	Name = "Hitbox color",
+	Description = "Changes the hitbox's color to your desired color",
+	Callback = function(color) 
+	_G.Color = color
+	end
+}
+
+Tab2:Dropdown{
+	Name = "Transparency",
+	StartingText = "Select...",
+	Description = "Changes your hitbox transparency",
+	Items = {
+		0,
+		0.5,-- {name, value}
+		0.10,
+		0.15,
+		0.20,
+		0.25,
+		0.35,
+		0.40,
+		0.45,
+		0.50,
+		0.55,
+		0.60,
+		0.65,
+		0.70,
+		0.75,
+		0.80,
+		0.85,
+		0.90,
+		0.95,
+		1,
+	},
+	Callback = function(item) _G.Transparency = item 
+	end
+}
+
+Tab2:Toggle{
+	Name = "Collision",
+	StartingState = false,
+	Description = "Toggle collision",
+	Callback = function(state) 
+	_G.Collision = state
+	end
+}
+
+-- MAIN BUTTON
+
+Tab:Button{
+	Name = "Execute",
+	Description = "Extends hitboxes",
+	Callback = function() 
+	-- notif
+GUI:Notification{
+	Title = "Success!",
+	Text = "Successfully extended hitboxes!",
+	Duration = 3,
+	Callback = function() end
+}
+-- script
+while wait(1) do
+for i,v in next, game.Players:GetPlayers() do
+if v.Name ~= game.Players.LocalPlayer.Name and v.Character then
+for i,d in next, getconnections(v.Character.Head.Changed) do
+d:Disable()
+end
+for i,c in next, getconnections(v.Character.Head:GetPropertyChangedSignal("Size")) do
+c:Disable()
+end
+pcall(function()
+ 
+v.Character.Head.Size = Vector3.new(_G.HeadSize, _G.HeadSize, _G.HeadSize)
+ 
+sethiddenproperty(v.Character.Head, "Massless", true)
+ 
+v.Character.Head.CanCollide = (_G.Collision)
+ 
+v.Character.Head.Transparency = (_G.Transparency)
+ 
+v.Character.Head.BrickColor = BrickColor.new(_G.Color)
+ 
+v.Character.Head.Material = ("Neon")
+ 
+end)
+end
+end
+end
+end
+}
+
